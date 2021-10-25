edc590917c7747eb1978d8811770a06484ee1c3c
Christopher Fanning <chris@chirstopherfanning.com>
2021-10-25 12:05:23 -0500

    Create Student class

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +18,12 @@ public class DemoApplication {
     }
 
     @GetMapping
-    public static String sayHello(){
-        return "Hello";
+    public static List<Student> sayHello(){
+        return List.of(new Student (1L,
+                "Bobby",
+                "thing@donk.com",
+                LocalDate.of(1990, 1, 13),
+                32
+        ));
     }
 }
